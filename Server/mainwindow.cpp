a9ddde2633f903278593a622e6434fa989a57913
Tomasz <pantomko@gmail.com>
2021-06-17 14:03:11 +0200

    handle peer disconnect

--- Server/mainwindow.cpp
+++ Server/mainwindow.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -30,18 +31,34 @@ void MainWindow::stop_server()
 
     for(auto & x : connections){
         x->close();
+        delete x;
     }
 }
-
 
 void MainWindow::accept_new_connection()
 {
     if(server.hasPendingConnections())
     {
         QTcpSocket *socket = server.nextPendingConnection();
-        std::cout << "peer : " << socket->socketDescriptor() << " connected !" << std::endl;
+        socket->setObjectName( QString::number(socket->socketDescriptor()) );
+
+        std::cout << "peer : " << socket->objectName().toStdString() << " connected !" << std::endl;
+
+        connect( socket, &QAbstractSocket::disconnected, this, &MainWindow::on_peer_disconnect );
 
         connected++;
         connections.push_back( socket );
     }
+}
+
+void MainWindow::on_peer_disconnect()
+{
+    QTcpSocket *socket = qobject_cast<QTcpSocket*>(sender());
+
+    std::cout << "peer : " << socket->objectName().toStdString() << " disconnected !" << std::endl;
+
+    connected--;
+    connections.erase( std::find_if(connections.begin(), connections.end(), [=]( auto x){ return x->objectName() == socket->objectName();} ));
+
+    delete socket;
 }
